631d68e47104f6dbe05a05014e7799249c7c9867
DienLe1291 <lvtdien.ag@gmail.com>
2023-05-24 09:14:58 +0700

    #DIEN add admin route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,8 @@ import watchRouter from './watch.route';
 const initRoutes = (app) => {
     app.use('/brand', brandRouter);
     app.use('/user', userRouter);
-<<<<<<< HEAD
     app.use('/watch', watchRouter);
-=======
     app.use('/admin', verifyAdmin, (req, res) => res.render('admin/home', {layout: 'admin'}))
->>>>>>> 18cc83310f9557655883e8c87b8ddd0fc26cfc1c
 }
 
 export default initRoutes;
