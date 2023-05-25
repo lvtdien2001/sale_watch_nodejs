7b8b6345fa68bdfedd2f8b83e2779abcd4443abc
DienLe1291 <lvtdien.ag@gmail.com>
2023-05-26 00:24:47 +0700

    #DIEN Xay dung header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,3 @@
-import { verifyAdmin } from '../middleware/auth';
 import adminRoute from './admin.route';
 import brandRouter from './brand.route';
 import userRouter from './user.route';
@@ -11,7 +10,29 @@ const initRoutes = (app) => {
     app.use('/admin', adminRoute);
     
     app.use('/:notfound', (req, res) => res.render('err404', {layout: false}))
-    app.use('/', (req, res) => res.render('home'))
+    app.use('/', (req, res) => res.render('home', {
+        helpers: {
+            getAuthBtn: () => {
+                if (req.session.authState){
+                    return ` <div class="dropdown">
+                                <button class="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fas fa-user"></i>
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li><a class="dropdown-item" href="#">#1</a></li>
+                                    <li><a class="dropdown-item" href="#">#2</a></li>
+                                    <li>
+                                        <a class="dropdown-item text-danger" href="/user/logout">
+                                            <i class="fas fa-sign-out-alt"></i> Đăng xuất
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>`
+                }
+                else return `<button class="header-btn-link"><a class="header-link" href="/user/login"><i class="fas fa-user"></i> Đăng nhập</a></button>`
+            }
+        }
+    }))
 }
 
 export default initRoutes;
