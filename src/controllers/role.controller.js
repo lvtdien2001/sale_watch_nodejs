2669708012a78b84dbba9748c2a10f0c4c8d3f50
Thanh17082001 <thienthanh17082001@gmail.com>
2023-05-30 13:16:23 +0700

    #Thanh chuc nang phan quyen nguoi dung

--- src/controllers/role.controller.js
+++ src/controllers/role.controller.js
@@ -1,77 +1,77 @@
 import roleService from '../services/role.service'
 
-import userService  from '../services/user.service'
+import userService from '../services/user.service'
 
-class roleController{
-    async updateRole(req, res){
+class roleController {
+    async updateRole(req, res) {
         try {
-          const roles = req.body.roles;
-          let data= []
-        for(let i=0 ; i<roles.length; i++){
-            data.push(
-                {
-                    roleId:roles[i]
-                }
-            )
-        }
-
-          if(roles){
-            const result = await userService.updateRole(req.params.id, data)
-            if(result){
-                res.redirect('/role')
+            const roles = req.body.roles;
+            let data = []
+            for (let i = 0; i < roles.length; i++) {
+                data.push(
+                    {
+                        roleId: roles[i]
+                    }
+                )
             }
-          }
-        } catch (error) {
-          console.log(error);
-        }
-        
-      }
 
-    async createRole(req, res){
+            if (roles) {
+                const result = await userService.updateRole(req.params.id, data)
+                if (result) {
+                    res.redirect('/role')
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    async createRole(req, res) {
         try {
-            if(req.body){
+            if (req.body) {
                 const result = await roleService.create(req.body)
-                if(result)
-                res.redirect('back')
+                if (result)
+                    res.redirect('back')
             }
         } catch (error) {
             console.log(error)
         }
     }
 
-    async displayRole(req, res){
+    async displayRole(req, res) {
         try {
             const allUser = await userService.findAllUser()
-            res.render('admin/role', {users: allUser, layout: 'admin'})
-            
+            res.render('admin/role', { users: allUser, layout: 'admin' })
+
         } catch (error) {
             console.log(error);
         }
     }
 
-    async roleUser(req, res){
+    async roleUser(req, res) {
         try {
             const userRoles = await userService.findById(req.params.id)
             const allrole = await roleService.findAll()
-            const roles=[]
-            for(let i=0 ; i<userRoles.roles.length; i++){
+            const roles = []
+            for (let i = 0; i < userRoles.roles.length; i++) {
                 roles.push(userRoles.roles[i].roleId.toObject())
             }
-            res.render('admin/role-update',{
-                    userRoles,
-                    listRoles:roles,
-                    layout:'admin',
-                    helpers:{
-                       templateRole: () =>{
-                        let inputChecked=''
-                        let inputCheck=''
-                        let temp=[]
+            res.render('admin/role-update', {
+                userRoles,
+                listRoles: roles,
+                layout: 'admin',
+                helpers: {
+                    templateRole: () => {
+                        let inputChecked = ''
+                        let inputCheck = ''
+                        let temp = []
 
-                        if(roles.length > 0){
-                            for(let i=0; i< allrole.length;i++){
-                                for(let j=0; j< roles.length; j++){
-                                    if(allrole[i].name == roles[j].name && !temp.includes(allrole[i].name)){
-                                        inputChecked+= `
+                        if (roles.length > 0) {
+                            for (let i = 0; i < allrole.length; i++) {
+                                for (let j = 0; j < roles.length; j++) {
+                                    if (allrole[i].name == roles[j].name && !temp.includes(allrole[i].name)) {
+                                        inputChecked += `
                                         <div>
                                             <input id="input__role" class="input__role" name="roles[]" type="checkbox" checked/ value="${allrole[i]._id}">
                                             <lable for="input__role">${allrole[i].name}</lable>
@@ -81,28 +81,28 @@ class roleController{
                                     }
                                 }
                             }
-            
-                            for(let i=0; i< temp.length;i++){
-                                for(let j=0; j< allrole.length; j++){
-                                    if(temp[i] != allrole[j].name){
-                                        inputCheck+= `
+
+                            for (let i = 0; i < temp.length; i++) {
+                                for (let j = 0; j < allrole.length; j++) {
+                                    if (temp[i] != allrole[j].name) {
+                                        inputCheck += `
                                         <div>
                                             <input id="input__role" name="roles[]" class="input__role" type="checkbox" value="${allrole[j]._id}"/>
                                             <lable for="input__role">${allrole[j].name}</lable>
                                         </div>
                                         `
                                     }
-                                    else{
+                                    else {
                                         i++
                                     }
                                 }
-                                
-                            } 
+
+                            }
                             // return allrole
                             return inputChecked + inputCheck;
-                        }else{
-                            for(let k=0; k<allrole.length; k++){    
-                                inputChecked+= ` 
+                        } else {
+                            for (let k = 0; k < allrole.length; k++) {
+                                inputChecked += ` 
                                         <div>
                                             <input id="input__role" name="roles[]" class="input__role" type="checkbox" value="${allrole[k]._id}" />
                                             <lable for="input__role" >${allrole[k].name}</lable>
@@ -111,49 +111,62 @@ class roleController{
                             }
                             return inputChecked;
                         }
-                       } 
                     }
                 }
+            }
             )
-            
+
         } catch (error) {
             console.log(error);
         }
     }
 
-    async displayCreateRole(req, res){
+    async displayCreateRole(req, res) {
         try {
             const roles = await roleService.findAll()
             res.render('admin/role-create', {
                 roles,
-                layout:'admin'
+                layout: 'admin'
             })
         } catch (error) {
             console.log(error);
         }
     }
 
-    async displayRoleEdit(req, res){
-        if(req.params.id){
+    async displayRoleEdit(req, res) {
+        if (req.params.id) {
             const role = await roleService.findById(req.params.id)
-            res.render('admin/role-edit',{
+            res.render('admin/role-edit', {
                 role,
-                layout:'admin'
+                layout: 'admin'
             })
         }
     }
 
-    async roleEdit(req, res){
+    async roleEdit(req, res) {
         try {
-            if(req.body){
-                const result = await roleService.updateRole(req.params.id,req.body)
-                if(result)
-                res.redirect(req.get('referer'))
-            }   
+            if (req.body) {
+                const result = await roleService.updateRole(req.params.id, req.body)
+                if (result)
+                    res.redirect(req.get('referer'))
+            }
         } catch (error) {
             console.log(error);
         }
     }
-} 
+
+    async deleteRole(req, res) {
+        try {
+            if (req.params.id) {
+                const result = await roleService.deleteRole(req.params.id)
+                if (result) {
+                    res.redirect(req.get('referer'))
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
 
 module.exports = new roleController
