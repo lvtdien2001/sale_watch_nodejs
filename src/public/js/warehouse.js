e4589307b8163d942ed9f6b436332a7dae6633fa
DienLe1291 <lvtdien.ag@gmail.com>
2023-06-15 16:41:49 +0700

    #DIEN Quan ly kho hang

--- src/public/js/warehouse.js
+++ src/public/js/warehouse.js
@@ -133,3 +133,18 @@ const changePage = (element, page) => {
     document.getElementById(`page-${prevPage}`).classList.toggle('active');
     document.getElementById(`page-${page}`).classList.toggle('active');
 };
+
+const printPDF = (index, date) => {
+    let mywindow = window.open('', 'PRINT', 'height=1123,width=900,top=100,left=150');
+
+    mywindow.document.write(`<html><head><title>Phieu-Nhap-Kho-${date}</title>`);
+    mywindow.document.write('</head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"><body >');
+    mywindow.document.write(document.getElementById(`content-print-${index}`).innerHTML);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+    mywindow.print();
+    mywindow.close();
+}
